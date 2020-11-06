3bbebc37b98e350fde056035b1f386c80d976480
aurelio <aureliodel.alumn@cescristorey.com>
2020-11-06 19:16:44 +0100

    actualizacion holamundo

--- holamundo.cpp
+++ holamundo.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int hola(int hola) {
+void hola() {
 cout << “Hola mundo” << endl;
-return hola;
 }
 int main() {
-string hola = NULL;
-int holaa = 9;
-holaa= hola(holaa);
+hola();
 return 0;
 }
+
 
